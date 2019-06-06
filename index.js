986fb5ef05b0f84ab28b0a89992443137459cd1e
Anton Everaert <antoneveraert@gmail.com>
2019-06-06 11:19:05 +0200

    added css + basic card function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const resultDiv = document.querySelector('#result');
+const cardsDiv =  document.querySelector('#cards'); 
 
 
 const pickRandom = () => {
@@ -8,3 +9,25 @@ const pickRandom = () => {
 };
 
 document.querySelector('#pickRandom').addEventListener('click', pickRandom);
+
+const beerCard = () => {
+	fetch('https://api.punkapi.com/v2/beers?page=1&per_page=20')
+		.then((response) => response.json())
+		.then((response) => {
+			for (let i = 0; i < response.length; i++) {  
+			cardsDiv.innerHTML += `
+				<div class="card" style="max-width: 200px; max-height: " >
+				  <img class="card-img-top" style="max-width: 100%;" src="${response[i].image_url}" alt="${response[i].name}">
+				  <div class="card-body">
+				    <h5 class="card-title">${response[i].name}</h5>
+				    <h6 class="card-subtitle mb-2 text-muted">${response[i].tagline}</h6>
+				    <p class="card-text">${response[i].description}</p>
+				  </div>
+				</div>
+			`;
+			} 
+		}
+		)
+	};
+
+beerCard(); 
